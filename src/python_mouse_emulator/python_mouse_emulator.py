b79ebf91ae1361165bd646c926603df415a4c080
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-05-18 21:56:12 +0200

    Added initial file for python

---
+++ src/python_mouse_emulator/python_mouse_emulator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class MouseEmulator:
+    """ Mouse emulator class. """
+
+    def __init__(self):
+        """ Initialize mouse emulator class. """
+        pass
+
+    def start(self):
+        """ Start the application. """
+        pass
+
+
+if __name__ == "__main__":
+    me = MouseEmulator()
+    me.start()
