b0d28b76850c356507f52ca736f0ccbd183b916d
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-05-20 21:55:58 +0200

    Split up into functions and created wait_for_start function and keep_alive_check function

--- src/arduino_mouse_emulator/arduino_mouse_emulator.ino
+++ src/arduino_mouse_emulator/arduino_mouse_emulator.ino
@@ -17,9 +17,15 @@
  *  Defines
  *************************/
 
-#define SENSOR_MIN_VALUE    0
-#define SENSOR_MAX_VALUE    1024
-#define MOUSE_STEP_OFFSET   100
+#define SENSOR_MIN_VALUE            0
+#define SENSOR_MAX_VALUE            1024
+#define MOUSE_STEP_OFFSET           100
+
+#define SENSOR_READING_TIMEOUT_MS   10
+#define KEEP_ALIVE_TIMEOUT_MS       10000
+
+#define START_MESSAGE               "0x1"
+#define KEEP_ALIVE_MESSAGE          "0x2"
 
 
 /**************************
@@ -31,17 +37,22 @@
  *  Global variables
  *************************/
 
-int horizontalPin = A7;                 /* Analog input pin for horizontal joystick value. */
-int verticalPin = A6;                   /* Analog input pin for vertical joystick value. */
-int switchPin = 9;                      /* Switch pin for the joystick. */
+int hPin = A7;              /* Analog input pin for horizontal joystick value. */
+int vPin = A6;              /* Analog input pin for vertical joystick value. */
+int swPin = 9;              /* Switch pin for the joystick. */
 
-int horizontalMidPos;                   /* The middle or natural position of the joystick horizontally. */
-int verticalMidPos;                     /* The middle or natural position of the joystick vertically. */
+int hMidPosValue;           /* The middle or neutral position value of the joystick horizontally. */
+int vMidPosValue;           /* The middle or neutral position value of the joystick vertically. */
+int hValue;                 /* The joystick horizontal value. */
+int vValue;                 /* The joystick vertical value. */
 
-int hStep;                              /* Stores the current horizontal step value. */
-int vStep;                              /* Stores the current vertical step value. */
-String hStepSign;                       /* Stores the current step value sign. */
-String vStepSign;                       /* Stores the current step value sign. */
+int hStep;                  /* Stores the current horizontal step value. */
+int vStep;                  /* Stores the current vertical step value. */
+String hStepSign;           /* Stores the current horizontal step value sign. */
+String vStepSign;           /* Stores the current vertical step value sign. */
+
+String keepAliveReceived;
+uint16_t keepAliveCounter = 0;
 
 
 /**********************************************************************************************************************
@@ -52,16 +63,16 @@ void setup()
     Serial.begin(9600);                 /* Start serial monitor. */
 
     /* Set analog pins as inputs (reads from the joystick sensor). */
-    pinMode(horizontalPin, INPUT);
-    pinMode(verticalPin, INPUT);
+    pinMode(hPin, INPUT);
+    pinMode(vPin, INPUT);
 
-    pinMode(switchPin, INPUT);          /* Set the switch pin as input. */
-    digitalWrite(switchPin, HIGH);      /* Pull switch pin high. */
+    pinMode(swPin, INPUT);              /* Set the switch pin as input. */
+    digitalWrite(swPin, HIGH);          /* Pull switch pin high. */
     delay(500);                         /* Short delay to let outputs settle */
 
     /* Initial joystick values (Joystick should be in neutral position when reading these). */
-    horizontalMidPos = analogRead(horizontalPin);
-    verticalMidPos = analogRead(verticalPin);
+    hMidPosValue = analogRead(hPin);
+    vMidPosValue = analogRead(vPin);
 }
 
 
@@ -70,14 +81,114 @@ void setup()
  *********************************************************************************************************************/
 void loop()
 {
-    /* Get mouse step values. */
-    hStep = translate_to_steps(analogRead(horizontalPin), horizontalMidPos);
-    vStep = translate_to_steps(analogRead(verticalPin), verticalMidPos);
+    /* Wait for a start message from the script. */
+    wait_for_start();
 
+    while (true)
+    {
+        if (!check_keep_alive())
+        {
+            break;
+        }
+
+        read_joystick_sensor();
+        calculate_steps();
+        set_step_signs();
+        tx_data_package();
+        delay(SENSOR_READING_TIMEOUT_MS);
+    }
+}
+
+
+/**********************************************************************************************************************
+ *  Wait for a start message from the script.
+ *********************************************************************************************************************/
+void wait_for_start()
+{
+    String received;
+
+    while (true)
+    {
+        if (Serial.available())
+        {
+            received = Serial.readString();
+
+            if (find_substring(received, (String) START_MESSAGE))
+            {
+                /* Start message received, let's start sensor reading and transmittion! */
+                break;
+            }
+        }
+    }
+}
+
+
+/**********************************************************************************************************************
+ *  A continuous check to see if a connection is still present between arduino and script.
+ *********************************************************************************************************************/
+bool check_keep_alive()
+{
+    if (keepAliveCounter >= KEEP_ALIVE_TIMEOUT_MS)
+    {
+        keepAliveCounter = 0;
+        /* No keep alive message have been received within the timeframe, go back to 'wait_for_start'. */
+        return false;
+    }
+
+    if (Serial.available())
+    {
+        keepAliveReceived = Serial.readString();
+
+        if (find_substring(keepAliveReceived, (String) KEEP_ALIVE_MESSAGE))
+        {
+            keepAliveCounter = 0;
+            return true;
+        }
+    }
+
+    keepAliveCounter += SENSOR_READING_TIMEOUT_MS;
+
+    return true;
+}
+
+
+/**********************************************************************************************************************
+ *  Reads the joystick sensor (horizontal and vertical data).
+ *********************************************************************************************************************/
+void read_joystick_sensor()
+{
+    hValue = analogRead(hPin);
+    vValue = analogRead(vPin);
+}
+
+
+/**********************************************************************************************************************
+ *  Calculates the steps based on the joystick sensor values.
+ *********************************************************************************************************************/
+void calculate_steps()
+{
+    /* Get mouse step values. */
+    hStep = translate_to_steps(hValue, hMidPosValue);
+    vStep = translate_to_steps(vValue, vMidPosValue);
+}
+
+
+/**********************************************************************************************************************
+ *  Sets the steps signs (+ or -).
+ *********************************************************************************************************************/
+void set_step_signs()
+{
     /* Set the step signs. */
     hStepSign = (hStep < 0) ? "" : "+";
     vStepSign = (vStep < 0) ? "" : "+";
+}
 
+
+/**********************************************************************************************************************
+ *  Transmits the steps data package via serial.
+ *********************************************************************************************************************/
+void tx_data_package()
+{
     /* 'S' indicate start, ':' seperate the step values, 'E' indicate end. */
     String str = "S" + hStepSign + ((String) hStep) + ":" + vStepSign + ((String) vStep) + "E";
 
@@ -116,4 +227,45 @@ int translate_to_steps(int value, int midPos)
     }
 
     return 0;
+}
+
+
+/**********************************************************************************************************************
+ *  Find a substring withing a string.
+ *********************************************************************************************************************/
+bool find_substring(String str, String substr)
+{
+    int strLength = str.length();
+    int substrLength = substr.length();
+
+    if (substrLength > strLength)
+    {
+        return false;
+    }
+
+    int i, j;
+    for (i = 0, j = 0; i != strLength; i++)
+    {
+        if (str[i] == substr[j])
+        {
+            if ((j + 1) == substrLength)
+            {
+                return true;
+            }
+            j++;
+        }
+        else
+        {
+            j = 0;
+        }
+    }
+
+    if ((j + 1) == substrLength)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
